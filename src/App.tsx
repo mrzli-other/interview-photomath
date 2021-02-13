3f08044c502dc50ffecd09bf00964eb97da52d08
Goran Mržljak <goran.mrzljak@gmail.com>
2021-02-13 17:31:57 +0100

    Implement expression calculator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function App(): React.ReactElement {
-  return <div>App</div>;
+  const [expression, setExpression] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+
+  return (
+    <div>
+      <div>
+        <textarea
+          value={expression}
+          onChange={(event) => {
+            setExpression(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            const result = await fetchResult(expression);
+            setResult(result);
+          }}
+          disabled={!expression}
+        >
+          Solve
+        </button>
+      </div>
+      <div>Result:</div>
+      <div>{result}</div>
+    </div>
+  );
+}
+
+async function fetchResult(expression: string): Promise<string> {
+  const url = new URL('http://api.mathjs.org/v4');
+  url.searchParams.append('expr', expression);
+  const response = await fetch(url.toString());
+  return response.text();
 }
